4804e5869f85ef3a107fb15dd30682c10c5cec0e
scottbae37 <scottbae37@gmail.com>
2019-01-07 08:13:28 +0900

    [Katas] Empty PrimeFactors kata added

---
+++ Katas/src/test/java/practice/PrimeFactorsTest.java
@@ -0,0 +1,62 @@
+package practice;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeFactorsTest {
+
+  @BeforeEach
+  void setUp() {
+    System.out.println("SETUP");
+  }
+
+  @AfterEach
+  void tearDown() {
+    System.out.println("tearDown");
+  }
+
+  @Test
+  public void of_1_is_none() {
+
+  }
+
+  @Test
+  public void of_2_is_2() {
+
+  }
+
+  @Test
+  public void of_3_is_3() {
+
+  }
+
+  @Test
+  public void of_4_is_4() {
+
+  }
+
+  @Test
+  public void of_6_is_2_3() {
+
+  }
+
+  @Test
+  public void of_8_is_2_2_2() {
+  }
+
+  @Test
+  public void of_9_is_3_3() {
+
+  }
+
+  @Test
+  public void acceptanceTest() {
+
+  }
+}
