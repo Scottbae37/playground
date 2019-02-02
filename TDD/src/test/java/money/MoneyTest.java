309acc0c9c2d3c984fa7d06c075d00fd9eccbcb4
scottbae37 <scottbae37@gmail.com>
2019-02-02 12:51:21 +0900

    [TDD] Money ch4 Privacy Refactoring

--- TDD/src/test/java/money/MoneyTest.java
+++ TDD/src/test/java/money/MoneyTest.java
@@ -10,7 +10,7 @@ public class MoneyTest {
    * TODO:
    * $5 + 10CHF = $10(환율이 2:1일 경우)
    * ---- $5 * 2 = $10
-   * amount를 private으로 만들기
+   * ---- amount를 private으로 만들기
    * ---- Dollar 부작용(side effect)?
    * Money 반올림?
    * ---- equals()
@@ -20,10 +20,8 @@ public class MoneyTest {
   @Test
   void testMultiplication() {
     Dollar five = new Dollar(5);
-    Dollar product = five.times(2);
-    assertEquals(10, product.amount);
-    product = five.times(3);
-    assertEquals(15, product.amount);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
   }
 
   @Test
@@ -33,7 +31,7 @@ public class MoneyTest {
   }
 
   private class Dollar {
-    public int amount;
+    private int amount;
 
     public Dollar(int amount) {
       this.amount = amount;
