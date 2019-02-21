16c89c00e7709738ee0a1f5d57714a129056f0c5
scottbae37 <scottbae37@gmail.com>
2019-02-21 16:28:29 +0900

    [TDD] Money ch11 Dollar/Franc and testDifferentClassEquality() removed

--- TDD/src/test/java/money/MoneyTest.java
+++ TDD/src/test/java/money/MoneyTest.java
@@ -22,7 +22,7 @@ public class MoneyTest {
    * ---- 공용 times
    * ---- Franc과 Dollar 비교하기
    * ---- 통화? (this.getClass().equals(money.getClass()))
-   * testFrancMultiplication 지워야 하나?
+   * ---- testFrancMultiplication 지워야 하나?
    */
 
   @Test
@@ -36,8 +36,6 @@ public class MoneyTest {
   void testEquality() {
     assertTrue(Money.dollar(5).equals(Money.dollar(5)));
     assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-    assertTrue(Money.franc(5).equals(Money.franc(5)));
-    assertFalse(Money.franc(5).equals(Money.franc(6)));
     assertFalse(Money.franc(5).equals(Money.dollar(5)));
   }
 
@@ -53,11 +51,6 @@ public class MoneyTest {
     assertEquals("USD", Money.dollar(1).currency());
     assertEquals("CHF", Money.franc(1).currency());
   }
-
-  @Test
-  void testDifferentClassEquality() {
-    assertTrue(new Money(10,"CHF").equals(new Franc(10, "CHF")));
-  }
 }
 
 class Money {
@@ -70,11 +63,11 @@ class Money {
   }
 
   static Money dollar(int amount) {
-    return new Dollar(amount, "USD");
+    return new Money(amount, "USD");
   }
 
   public static Money franc(int amount) {
-    return new Franc(amount, "CHF");
+    return new Money(amount, "CHF");
   }
 
   @Override
@@ -89,17 +82,5 @@ class Money {
 
   String currency() {
     return currency;
-  }
-}
-
-class Dollar extends Money {
-  Dollar(int amount, String currency) {
-    super(amount, currency);
-  }
-}
-
-class Franc extends Money {
-  Franc(int amount, String currency) {
-    super(amount, currency);
   }
 }
