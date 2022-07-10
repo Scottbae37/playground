8331496cc1c3418afb34fc3595cce7c73bf7e63b
7110992 <jaeseung.bae@hyundai.com>
2022-07-11 08:10:40 +0900

    dfs permutation

--- python/algo/leetcode/graph/46_permute_dfs.py
+++ python/algo/leetcode/graph/46_permute_dfs.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import *
 
 
@@ -21,7 +22,15 @@ class Solution:
         return result
 
 
+class Solution2:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(itertools.permutations(nums))
+
+
+
 if __name__ == '__main__':
     assert [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]] == Solution().permute([1, 2, 3])
     assert [[0, 1], [1, 0]] == Solution().permute([0, 1])
     assert [[1]] == Solution().permute([1])
+
+    assert [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]] == Solution2().permute([1, 2, 3])
